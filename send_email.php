200bc34e559ab5a02b0704f92cb95d9aa98d7094
Ahmed ABDI DIRRANE <152913736+Ahmed-Abdi-creator@users.noreply.github.com>
2024-01-04 17:04:50 +0100

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,1 +1,16 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
 
+    $to = "ahmadialgie34@gmail.com";
+    $subject = "Nouvelle demande de formulaire de contact";
+    $headers = "From: $email \r\n";
+    $headers .= "Content-type: text/html\r\n";
+
+    $body = "Nom: $name <br> Email: $email <br> Message: $message";
+
+    mail($to, $subject, $body, $headers);
+}
+?>
